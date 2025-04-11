31b8dd3471a129d1ba810be8a2fae93b6ad0d436
centy29 <vincesmoke666@gmail.com>
2025-04-11 11:25:03 +0800

    Create ReceiptInfo.cs

---
+++ ReceiptDataLayer/ReceiptInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptDataLayer
+{
+    class accountdetails
+    {
+        public string pin { get; set; }
+        public string name { get; set; }
+        
+    }
+}
