baadc349f82139ed75ff9ada3de2cf1d24c9243a
centy29 <vincesmoke666@gmail.com>
2025-06-06 18:32:44 +0800

    Create IReceiptAccounts.cs

---
+++ ReceiptDataLayer/IReceiptAccounts.cs
@@ -0,0 +1,22 @@
+﻿using ReceiptCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptDataLayer
+{
+    public interface IReceiptAccounts
+    {
+        List<ReceiptAccounts> GetAccounts();
+        public void createaccount(ReceiptAccounts account);
+        public void retrieveaccount(ReceiptAccounts account) { }
+        public void removeaccount(ReceiptAccounts account);
+        public void updateaccount(ReceiptAccounts account);
+        bool ValidateAccount(string pin);
+        string Getusername(string pin);
+
+
+    }
+}
