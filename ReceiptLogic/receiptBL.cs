340d3d9e611076524ca7ce3699a56fe3d400824c
centy29 <vincesmoke666@gmail.com>
2025-06-06 18:37:18 +0800

    Update receiptBL.cs

--- ReceiptLogic/receiptBL.cs
+++ ReceiptLogic/receiptBL.cs
@@ -8,18 +8,21 @@ namespace recieptlogic
 {
     public class recieptbusinessdata
     {
-        public static string[] options = { "1. ADD EXPENSES", "2. PAYMENT METHOD", "3. HISTORY", "4.UPDATE" , "5.EXIT" }; // options for the user
+        public static string[] options = { "1. ADD EXPENSES", "2. PAYMENT METHOD", "3. HISTORY", "4.UPDATE", "5.EXIT" }; // options for the user
 
         public static List<(string brand, string address, int tin, int invoice, double amount)> receipts = new List<(string, string, int, int, double)>();//here store the add expenses input\
 
         public static bool removemethod(int invoice)
         {
-            for (int i = 0; i < receipts.Count; i++ ) {
-                if (receipts[i].invoice == invoice){ //changed the method, now it tracks the invoice to select what to remove
+            for (int i = 0; i < receipts.Count; i++)
+            {
+                if (receipts[i].invoice == invoice)
+                { //changed the method, now it tracks the invoice to select what to remove
                     receipts.RemoveAt(i);
                     return true;
+                }
             }
-          } return false;//next update:  notifies the user the details of the selected invoice
+            return false;//next update:  notifies the user the details of the selected invoice
         }
 
         public static bool UpdateReceipt(int invoice, string newBrand, string newAddress, int newTin, double newAmount)
