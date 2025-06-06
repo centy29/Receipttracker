07156fa2e31527553e61587ffe2c59fa48944639
centy29 <vincesmoke666@gmail.com>
2025-06-06 18:32:53 +0800

    Update ReceiptDataLayer.AssemblyInfo.cs

--- ReceiptDataLayer/obj/Debug/net8.0/ReceiptDataLayer.AssemblyInfo.cs
+++ ReceiptDataLayer/obj/Debug/net8.0/ReceiptDataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReceiptDataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4345711a14a68c12c847b4e971277a9818c4c3e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63559a230247aafe37989813ce37573ccc09f3f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReceiptDataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReceiptDataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
