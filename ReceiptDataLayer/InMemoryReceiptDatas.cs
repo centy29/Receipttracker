de3ec088a49f6b296af5fd20153569d489fcd10f
centy29 <vincesmoke666@gmail.com>
2025-06-06 18:32:41 +0800

    Create InMemoryReceiptDatas.cs

---
+++ ReceiptDataLayer/InMemoryReceiptDatas.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReceiptCommon;
+namespace ReceiptDataLayer
+{
+    public class InMemoryReceiptDatas : IReceiptAccounts
+    {
+
+
+        public List<ReceiptAccounts> accounts = new List<ReceiptAccounts>();
+        public List<ReceiptAccounts> GetAccounts()
+        {
+            return accounts;
+        }
+        public void createaccount(ReceiptAccounts account)
+        {
+
+        }
+        public void removeaccount(ReceiptAccounts account)
+        {
+
+        }
+        public void updateaccount(ReceiptAccounts account)
+        {
+
+        }
+        public void retrieveaccount(ReceiptAccounts account)
+        {
+
+        }
+        public InMemoryReceiptDatas()
+        {
+            createdummyaccount();
+        }
+        private void createdummyaccount()
+        {
+
+            accounts.Add(new ReceiptAccounts
+            {
+                name = "vincent",
+                pin = "centy"
+
+            });
+
+            accounts.Add(new ReceiptAccounts
+            {
+                name = "zyrah",
+                pin = "zj"
+
+            });
+        }
+
+        public bool ValidateAccount(string pin)// validates the pin of the accounts that is in the list
+        {
+            foreach (var account in accounts)
+            {
+                if (account.pin == pin)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public string Getusername(string pin)
+        { // method for the pin gets the name in the account
+
+            foreach (var account in accounts)
+            {
+
+                if (account.pin == pin)
+                {
+
+                    return account.name;
+                }
+            }
+            return null;
+        }
+
+
+
+
+    }
+}
