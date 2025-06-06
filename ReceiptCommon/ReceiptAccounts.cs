73a5299c9a1244fbca03477273a7b782446370e0
centy29 <vincesmoke666@gmail.com>
2025-06-06 18:32:22 +0800

    Create ReceiptAccounts.cs

---
+++ ReceiptCommon/ReceiptAccounts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptCommon
+{
+    public class ReceiptAccounts
+    {
+        public string pin { get; set; }
+        public string name { get; set; }
+
+
+    }
+}
