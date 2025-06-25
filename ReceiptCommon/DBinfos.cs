1abc46e007959790c3ad96ce6140a02bef1cbdd1
centy29 <vincesmoke666@gmail.com>
2025-06-26 04:00:00 +0800

    Create DBinfos.cs

---
+++ ReceiptCommon/DBinfos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptCommon
+{
+    public class DBinfos
+    {
+        public int invoice { get; set; }
+        public string brand { get; set; }
+        public string address { get; set; }
+        public int tin { get; set; }
+        public decimal amount { get; set; }
+       
+    }
+}
