7c2d67213d3bee2f45c40107609188bc805851cc
centy29 <vincesmoke666@gmail.com>
2025-06-27 19:05:14 +0800

    Create APIReceipt.cs

---
+++ ReceiptCommon/APIReceipt.cs
@@ -0,0 +1,11 @@
+﻿namespace ReceiptCommon
+{
+    public class APIReceipt
+    {
+        public string Brand { get; set; }
+        public string Address { get; set; }
+        public int Tin { get; set; }
+        public int Invoice { get; set; }
+        public double Amount { get; set; }
+    }
+}
