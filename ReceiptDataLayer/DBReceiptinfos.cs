87ad286d52ad5b2cd68ec33fc836cf14d3ddb54b
centy29 <vincesmoke666@gmail.com>
2025-06-27 19:11:12 +0800

    Update DBReceiptinfos.cs

--- ReceiptDataLayer/DBReceiptinfos.cs
+++ ReceiptDataLayer/DBReceiptinfos.cs
@@ -1,132 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ReceiptCommon;
-using Microsoft.Data.Sql;
+using System.Data;
 using Microsoft.Data.SqlClient;
+using ReceiptCommon;
 
 namespace ReceiptDataLayer
 {
-   public class DBReceiptinfo
+    public class DBReceiptinfo
     {
+        private readonly string connectionString = "Data Source=centy\\SQLEXPRESS;Initial Catalog=DBReceiptDatas;Integrated Security=True;TrustServerCertificate=True;";
 
-        static string connectionString   // Connection string for SQL Server connection
-      = "Data Source =centy\\SQLEXPRESS; Initial Catalog = DBReceiptDatas; Integrated Security = True; TrustServerCertificate=True;";
-
-        static SqlConnection sqlConnection;
-
-        public DBReceiptinfo()
+        public List<DBinfos> GetAllRecieptInfos()
         {
-            sqlConnection = new SqlConnection(connectionString);
-        }
-        public List<DBinfos> receipt = new List<DBinfos>();
-
-        public List<DBinfos> GetAllRecieptInfos ()  // Method to retrieve all accounts from the Receipt table
-        {
-            string selectQuery = "SELECT invoice, brand, address, tin, amount FROM ReceiptsInfo";
-            SqlCommand command = new SqlCommand(selectQuery, sqlConnection);
-
-            sqlConnection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-
-            List<DBinfos> infos = new List<DBinfos>();
-
-            while (reader.Read())
+            var infos = new List<DBinfos>();
+            using (var conn = new SqlConnection(connectionString))
             {
-                infos.Add(new DBinfos
+                var query = "SELECT invoice, brand, address, tin, amount FROM ReceiptsInfo";
+                var command = new SqlCommand(query, conn);
+
+                conn.Open();
+                using (var reader = command.ExecuteReader())
                 {
-                   invoice = Convert.ToInt32(reader["invoice"]),
-                    brand = reader["brand"].ToString(),
-                    address = reader["address"].ToString(),
-                    tin = Convert.ToInt32(reader["tin"]),
-                    amount = Convert.ToDecimal(reader["amount"]),
-                  
-
-
-                });
+                    while (reader.Read())
+                    {
+                        infos.Add(new DBinfos
+                        {
+                            invoice = Convert.ToInt32(reader["invoice"]),
+                            brand = reader["brand"].ToString(),
+                            address = reader["address"].ToString(),
+                            tin = Convert.ToInt32(reader["tin"]),
+                            amount = Convert.ToDecimal(reader["amount"])
+                        });
+                    }
+                }
             }
-
-            sqlConnection.Close();
             return infos;
         }
+
         public DBinfos GetInvoice(int invoice)
         {
-            string query = "SELECT invoice, brand, address, tin, amount FROM ReceiptsInfo WHERE invoice = @invoice";
-            SqlCommand command = new SqlCommand(query, sqlConnection);
-            command.Parameters.AddWithValue("@invoice", invoice);
-
-
-            sqlConnection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-
-            DBinfos receipt = null;
-
-            if (reader.Read())
+            using (var conn = new SqlConnection(connectionString))
             {
-                receipt = new DBinfos
+                var query = "SELECT invoice, brand, address, tin, amount FROM ReceiptsInfo WHERE invoice = @invoice";
+                var command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@invoice", invoice);
+
+                conn.Open();
+                using (var reader = command.ExecuteReader())
                 {
-                    invoice = Convert.ToInt32(reader["invoice"]),
-                    brand = reader["brand"].ToString(),
-                    address = reader["address"].ToString(),
-                    tin = Convert.ToInt32(reader["tin"]),
-                    amount = Convert.ToDecimal(reader["amount"]),
-                    
-                };
-               
+                    if (reader.Read())
+                    {
+                        return new DBinfos
+                        {
+                            invoice = Convert.ToInt32(reader["invoice"]),
+                            brand = reader["brand"].ToString(),
+                            address = reader["address"].ToString(),
+                            tin = Convert.ToInt32(reader["tin"]),
+                            amount = Convert.ToDecimal(reader["amount"])
+                        };
+                    }
+                }
             }
-            sqlConnection.Close();
-            return receipt;
+            return null;
         }
-        public void addreceiptinfo(DBinfos receipt)
+
+        public bool AddReceipt(DBinfos receipt)
         {
-            string insertQuery = "INSERT INTO ReceiptsInfo (invoice, brand, address, tin, amount) VALUES (@invoice, @brand, @address, @tin, @amount)";
-            SqlCommand command = new SqlCommand(insertQuery, sqlConnection);
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var query = "INSERT INTO ReceiptsInfo (invoice, brand, address, tin, amount) VALUES (@invoice, @brand, @address, @tin, @amount)";
+                var command = new SqlCommand(query, conn);
 
-            command.Parameters.AddWithValue("@invoice", receipt.invoice);
-            command.Parameters.AddWithValue("@brand",receipt.brand );
-            command.Parameters.AddWithValue("@address", receipt.address);
-            command.Parameters.AddWithValue("@tin", receipt.tin);
-            command.Parameters.AddWithValue("@amount", receipt.amount);
-           
+                command.Parameters.AddWithValue("@invoice", receipt.invoice);
+                command.Parameters.AddWithValue("@brand", receipt.brand);
+                command.Parameters.AddWithValue("@address", receipt.address);
+                command.Parameters.AddWithValue("@tin", receipt.tin);
+                command.Parameters.AddWithValue("@amount", receipt.amount);
 
-            sqlConnection.Open();
-            command.ExecuteNonQuery();
-            sqlConnection.Close();
+                conn.Open();
+                return command.ExecuteNonQuery() > 0;
+            }
         }
-        public void deleteReceiptInfo(DBinfos receipt)
+
+        public bool UpdateReceipt(DBinfos receipt)
         {
-            string deleteQuery = "DELETE FROM ReceiptsInfo WHERE invoice = @invoice";
-            SqlCommand command = new SqlCommand(deleteQuery, sqlConnection);
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var query = "UPDATE ReceiptsInfo SET brand = @brand, address = @address, tin = @tin, amount = @amount WHERE invoice = @invoice";
+                var command = new SqlCommand(query, conn);
 
-            command.Parameters.AddWithValue("@invoice", receipt.invoice);
+                command.Parameters.AddWithValue("@invoice", receipt.invoice);
+                command.Parameters.AddWithValue("@brand", receipt.brand);
+                command.Parameters.AddWithValue("@address", receipt.address);
+                command.Parameters.AddWithValue("@tin", receipt.tin);
+                command.Parameters.AddWithValue("@amount", receipt.amount);
 
-            sqlConnection.Open();
-            command.ExecuteNonQuery();
-            sqlConnection.Close();
+                conn.Open();
+                return command.ExecuteNonQuery() > 0;
+            }
         }
-        public void updatereceiptinfo(DBinfos receipt)
+
+        public bool DeleteReceipt(int invoice)
         {
-            string updateQuery = "UPDATE ReceiptsInfo SET brand = @brand, address = @address, tin = @tin, amount = @amount WHERE invoice = @invoice";
-            SqlCommand command = new SqlCommand(updateQuery, sqlConnection);
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var query = "DELETE FROM ReceiptsInfo WHERE invoice = @invoice";
+                var command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@invoice", invoice);
 
-            command.Parameters.AddWithValue("@invoice", receipt.invoice);
-            command.Parameters.AddWithValue("@brand", receipt.brand);
-            command.Parameters.AddWithValue("@address", receipt.address);
-            command.Parameters.AddWithValue("@tin", receipt.tin);
-            command.Parameters.AddWithValue("@amount", receipt.amount);
-            
-
-            sqlConnection.Open();
-            command.ExecuteNonQuery();
-            sqlConnection.Close();
+                conn.Open();
+                return command.ExecuteNonQuery() > 0;
+            }
         }
-        public void retrieveaccount(ReceiptAccounts account)
-        {
-
-        }
-  
-
     }
 }
