58e20f5662b3522ae11c7f9c6f353cc189248380
centy29 <vincesmoke666@gmail.com>
2025-06-27 19:09:32 +0800

    Create Receiptcontrollers.cs

---
+++ ReceiptController/Controllers/Receiptcontrollers.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReceiptCommon;
+using ReceiptLogic;
+using System.Collections.Generic;
+
+namespace ReceiptTrackerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReceiptController : ControllerBase
+    {
+        private readonly ReceiptService _service = new ReceiptService();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<DBinfos>> GetAll()
+        {
+            var list = _service.GetAll();
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody] DBinfos receipt)
+        {
+            if (_service.Add(receipt))
+                return Ok("New receipt saved.");
+            return BadRequest("Could not save receipt.");
+        }
+
+        [HttpPatch("{invoice}")]
+        public ActionResult Update(int invoice, [FromBody] DBinfos receipt)
+        {
+            if (_service.Update(invoice, receipt))
+                return Ok("Receipt updated successfully.");
+            return NotFound("Receipt not found.");
+        }
+
+        [HttpDelete("{invoice}")]
+        public ActionResult Delete(int invoice)
+        {
+            if (_service.Remove(invoice))
+                return Ok("Receipt removed.");
+            return NotFound("Invoice does not exist.");
+        }
+    }
+}
