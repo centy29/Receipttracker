e36b1b0eedefe870d30b1606dd4bc7253dd78516
centy29 <vincesmoke666@gmail.com>
2025-06-28 00:35:01 +0800

    Update ReceiptTracker.AssemblyInfo.cs

--- ReceiptTracker/obj/Debug/net8.0/ReceiptTracker.AssemblyInfo.cs
+++ ReceiptTracker/obj/Debug/net8.0/ReceiptTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReceiptTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5b149175169582a1b601179a36618ed282f282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b9891c26f53e39d0a59951a09580ffc425d3304")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReceiptTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReceiptTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
