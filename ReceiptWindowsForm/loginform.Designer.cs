7c02355fc02023477564028c4bdc239f4a161569
centy29 <vincesmoke666@gmail.com>
2025-06-28 00:37:38 +0800

    Create loginform.Designer.cs

---
+++ ReceiptWindowsForm/loginform.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ReceiptWindowsForm
+{
+    partial class loginform
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(206, 285);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 41);
+            button1.TabIndex = 0;
+            button1.Text = "LOGIN";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += loginbtn;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Location = new Point(425, 285);
+            button2.Name = "button2";
+            button2.Size = new Size(123, 41);
+            button2.TabIndex = 1;
+            button2.Text = "REGISTER";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += registerbtn;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(305, 29);
+            label1.Margin = new Padding(10, 0, 10, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(154, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Welcome To Receipt Tracker";
+            label1.Click += welcomelbl;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(359, 153);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 23);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += inputpintxt;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.Location = new Point(225, 153);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 23);
+            label2.TabIndex = 4;
+            label2.Text = "ENTER PIN: ";
+            label2.Click += pinlbl;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button3.Location = new Point(643, 395);
+            button3.Name = "button3";
+            button3.Size = new Size(92, 29);
+            button3.TabIndex = 5;
+            button3.Text = "EXIT";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += exitbtn;
+            // 
+            // loginform
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(803, 453);
+            Controls.Add(button3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "loginform";
+            Text = "LOG-IN";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button3;
+    }
+}
