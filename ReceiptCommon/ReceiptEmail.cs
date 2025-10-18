425acd1078457649636002b46a73883f8be9ec0d
centy29 <vincesmoke666@gmail.com>
2025-10-18 10:01:51 +0800

    Create ReceiptEmail.cs

---
+++ ReceiptCommon/ReceiptEmail.cs
@@ -0,0 +1,10 @@
+﻿namespace ReceiptCommon
+{
+    public class ReceiptEmail
+    {
+        public string FromEmail { get; set; }
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
