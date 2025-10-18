6f791df852ad39c41213f54696239063b9fefe04
centy29 <vincesmoke666@gmail.com>
2025-10-18 10:21:33 +0800

    Create ReceiptEmailLogic.cs

---
+++ ReceiptLogic/ReceiptEmailLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using ReceiptCommon;
+
+namespace recieptlogicemail
+{
+    public class EmailLogic
+    {
+        public string SendEmail(ReceiptEmail info)
+        {
+            try
+            {
+                var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+                {
+                    Credentials = new NetworkCredential("58e5ac477b1ac0", "81f429603c134d"),
+                    EnableSsl = true
+                };
+
+                client.Send(info.FromEmail, info.ToEmail, info.Subject, info.Body);
+                return "Email Sent Successfully!";
+            }
+            catch (Exception ex)
+            {
+                return "Failed to send email: " + ex.Message;
+            }
+        }
+    }
+}
